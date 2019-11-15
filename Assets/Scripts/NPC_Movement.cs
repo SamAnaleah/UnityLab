4c5e34a73bd064173320140ec61f974a8c8548a0
Samantha Blanc <samantha.blanc@my.uwi.edu>
2019-11-15 17:50:56 -0400

    Second

---
+++ Assets/Scripts/NPC_Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Movement : MonoBehaviour
+{
+	Rigidbody2D rigidbody2D;
+	
+	float speed = 1.5f;
+	int direction = 1;
+	float timer;
+	float movementTime = 4.0f;
+	
+	Animator animator;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+		timer = movementTime;
+		animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = rigidbody2D.position;
+		timer -= Time.deltaTime;
+		
+		if(timer < 0)
+		{
+			direction = -direction;
+			timer = movementTime;
+			animator.SetFloat("MoveX", direction);
+		}
+		
+		position.x = position.x + speed * direction * Time.deltaTime;
+		rigidbody2D.MovePosition(position);
+    }
+}
---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+	Rigidbody2D rigidbody2D;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 60;
+		rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+        Vector2 position = rigidbody2D.position;
+		position.x = position.x + 1f * horizontal * Time.deltaTime;
+		position.y = position.y + 1f * vertical * Time.deltaTime;
+		rigidbody2D.MovePosition(position);
+    }
+}
